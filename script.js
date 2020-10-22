a1c195f40678566a0649d7551e8876f033035093
Kayleigh Shaw <shaw.kayleighemma@gmail.com>
2020-10-22 16:56:33 +0100

    Reasons changed in script.js

--- script.js
+++ script.js
@@ -4,17 +4,27 @@ function randomArrayItem(array) {
 
 // All the functionality of this page is copied from a different project I did a couple of days earlier, so some variable names etc. might seem irrelevant.
 const reasonsArray = [
-    "Learn more of the language that you're currently learning!",
-    "Learn more of the other language that you're currently learning!",
-    "Practise your main musical instrument!",
+    // "Learn more of the language that you're currently learning!",
+    // "Learn more of the other language that you're currently learning!",
+    // "Practise your main musical instrument!",
     "Read!",
     "Tidy!",
-    "Do some coding!",
-    "Do some arts and/or crafts!",
-    "Do some exercise!"
+    "Practice some drawing!",
+    // "Do some coding!",
+    // "Do some arts and/or crafts!",
+    // "Do some exercise!"
+    "Do 30 minutes of Ring Fit!",
+    "Learn some Japanese!",
+    "Practice some JavaScript!",
+    "Work on text-based Python game!",
+    "Go for a walk!",
+    "Do some writing!",
+    "Do some editing!",
+    "Go shopping!",
+    "Play some games :D"
 ];
 
-// alternative colours
+// alternative Colette colours
 // const coloursArray = [
 //     "#33a8c7",
 //     "#52e3e1",
@@ -27,6 +37,34 @@ const reasonsArray = [
 //     "#9336fd"
 // ]
 
+//Colette colours
+// const coloursArray = [
+//     "#362343",
+//     "#27061a",
+//     "#501010",
+//     "#3e010c",
+//     // "#703301",
+//     // "#735203",
+//     // "#404727",
+//     "#26391F",
+//     "#193d3f",
+//     "#1a2d46",
+//     "#003d5a",
+//     "#061f25",
+//     // "#223538",
+//     "#264D53",
+//     "#0a4346",
+//     // "#2d4f31",
+//     "#1E3C22",
+//     "#272640",
+//     "#312244",
+//     "#3e1f47",
+//     "#4d194d",
+//     "#290025",
+//     "#780c56"
+// ]
+
+//Kayleigh colours
 const coloursArray = [
     "#362343",
     "#27061a",
